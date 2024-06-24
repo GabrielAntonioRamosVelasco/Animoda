ce01d29401b211063d2d536616a01a0f90ea33c1
GabrielAntonioRamosVelasco <ramos.velasco.gabriel.antonio@gmail.com>
2024-06-23 21:31:44 -0600

    Carrito final v2

--- src/main/java/com/tienda/animoda/Controllers/CarritoController.java
+++ src/main/java/com/tienda/animoda/Controllers/CarritoController.java
@@ -5,6 +5,7 @@ import com.tienda.animoda.Entities.CarritoItem;
 import com.tienda.animoda.Entities.Inventario;
 import com.tienda.animoda.Entities.User;
 import com.tienda.animoda.Repositories.CarritoRepository;
+import com.tienda.animoda.Repositories.CarritoItemRepository;
 import com.tienda.animoda.Repositories.InventarioRepository;
 import com.tienda.animoda.Repositories.UserRepository;
 import com.tienda.animoda.dto.AddProductRequest;
@@ -23,6 +24,9 @@ public class CarritoController {
 
     @Autowired
     private CarritoRepository carritoRepository;
+
+    @Autowired
+    private CarritoItemRepository carritoItemRepository;
 
     @Autowired
     private UserRepository userRepository;
@@ -88,5 +92,102 @@ public class CarritoController {
         return ResponseEntity.ok(carrito);
     }
 
-    // Otros endpoints según sea necesario
+    @DeleteMapping("/removeProduct/{productId}")
+    public ResponseEntity<?> removeProductFromCarrito(@PathVariable Long productId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        if (userOptional.isEmpty()) {
+            return ResponseEntity.status(403).body("User not found");
+        }
+        User user = userOptional.get();
+
+        Carrito carrito = carritoRepository.findByUser(user);
+        if (carrito == null) {
+            return ResponseEntity.status(404).body("Carrito not found");
+        }
+
+        Optional<CarritoItem> itemOptional = carrito.getItems().stream()
+                .filter(item -> item.getProduct().getId_producto().equals(productId))
+                .findFirst();
+
+        if (itemOptional.isPresent()) {
+            CarritoItem item = itemOptional.get();
+            carrito.getItems().remove(item);
+            carritoItemRepository.delete(item);
+            carritoRepository.save(carrito);
+        } else {
+            return ResponseEntity.status(404).body("Product not found in carrito");
+        }
+
+        return ResponseEntity.ok(carrito);
+    }
+
+    @PostMapping("/increaseQuantity/{productId}")
+    public ResponseEntity<?> increaseProductQuantity(@PathVariable Long productId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        if (userOptional.isEmpty()) {
+            return ResponseEntity.status(403).body("User not found");
+        }
+        User user = userOptional.get();
+
+        Carrito carrito = carritoRepository.findByUser(user);
+        if (carrito == null) {
+            return ResponseEntity.status(404).body("Carrito not found");
+        }
+
+        Optional<CarritoItem> itemOptional = carrito.getItems().stream()
+                .filter(item -> item.getProduct().getId_producto().equals(productId))
+                .findFirst();
+
+        if (itemOptional.isPresent()) {
+            CarritoItem item = itemOptional.get();
+            item.setQuantity(item.getQuantity() + 1);
+            carritoItemRepository.save(item);
+        } else {
+            return ResponseEntity.status(404).body("Product not found in carrito");
+        }
+
+        return ResponseEntity.ok(carrito);
+    }
+
+    @PostMapping("/decreaseQuantity/{productId}")
+    public ResponseEntity<?> decreaseProductQuantity(@PathVariable Long productId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        if (userOptional.isEmpty()) {
+            return ResponseEntity.status(403).body("User not found");
+        }
+        User user = userOptional.get();
+
+        Carrito carrito = carritoRepository.findByUser(user);
+        if (carrito == null) {
+            return ResponseEntity.status(404).body("Carrito not found");
+        }
+
+        Optional<CarritoItem> itemOptional = carrito.getItems().stream()
+                .filter(item -> item.getProduct().getId_producto().equals(productId))
+                .findFirst();
+
+        if (itemOptional.isPresent()) {
+            CarritoItem item = itemOptional.get();
+            if (item.getQuantity() > 1) {
+                item.setQuantity(item.getQuantity() - 1);
+                carritoItemRepository.save(item);
+            } else {
+                carrito.getItems().remove(item);
+                carritoItemRepository.delete(item);
+            }
+        } else {
+            return ResponseEntity.status(404).body("Product not found in carrito");
+        }
+
+        return ResponseEntity.ok(carrito);
+    }
 }
